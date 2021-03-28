725ba8734aad7aa79b693b2688f6337ae1c16343
Manish Kumar <37872019+manish11414@users.noreply.github.com>
2021-03-28 20:27:35 +0530

    Delete BlogApplication directory

--- BlogApplication/src/main/java/com/springboot/blog/service/PostTagService.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.blog.service;
-
-import com.springboot.blog.entity.PostTag;
-
-public interface PostTagService {
-    void addNewTag(PostTag newTag);
-    PostTag getTagByName(String tagName);
-}
--- BlogApplication/src/main/java/com/springboot/blog/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.blog.service;
-
-import com.springboot.blog.entity.User;
-import com.springboot.blog.dto.UserRegistrationDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService{
-	User save(UserRegistrationDto registrationDto);
-}
